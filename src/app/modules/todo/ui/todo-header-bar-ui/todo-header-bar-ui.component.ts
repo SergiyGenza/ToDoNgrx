f11b2c6a2150d06ff54141e10a63efcf678f55f7
SergiyGenza <63913766+SergiyGenza@users.noreply.github.com>
2024-08-16 17:02:25 +0300

    add clear btn for demo

--- src/app/modules/todo/ui/todo-header-bar-ui/todo-header-bar-ui.component.ts
+++ src/app/modules/todo/ui/todo-header-bar-ui/todo-header-bar-ui.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Inject, Input, Output } from '@angular/core';
 import { Category } from '../../common/models/category.model';
+import { LocalstorageService } from '../../common/services/localstorage.service';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-todo-header-bar-ui',
@@ -11,7 +13,10 @@ export class TodoHeaderBarUiComponent {
   @Input() currentCategory?: Category | null;
   @Output() currentCategoryEmmiter = new EventEmitter<Category | null>();
 
-  constructor() { }
+  constructor(
+    private localStorageService: LocalstorageService,
+    @Inject(DOCUMENT) private _document: Document
+  ) { }
 
   public onCategoryPick(catagory: Category): void {
     this.currentCategory = catagory;
@@ -21,5 +26,10 @@ export class TodoHeaderBarUiComponent {
   public onAllCategoriesPick(): void {
     this.currentCategory = null;
     this.currentCategoryEmmiter.emit(null);
+  }
+
+  public clearAllData(): void {
+    this.localStorageService.clearAllData();
+    this._document.defaultView!.location.reload();
   }
 }

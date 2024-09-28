e5fdb4310edfc7125d56ef5e41a4aa7cebb263db
SergiyGenza <63913766+SergiyGenza@users.noreply.github.com>
2024-09-28 21:44:57 +0300

    ref: replaced old svg component with new one

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,24 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SvgIconComponent } from '../todo/ui/svg-icon/svg-icon.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormItemComponent } from './forms/form-item/form-item.component';
 import { EditFormComponent } from './forms/edit-form/edit-form.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 
 
 @NgModule({
   declarations: [
-    SvgIconComponent,
     FormItemComponent,
     EditFormComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    HttpClientModule,
+    AngularSvgIconModule.forRoot()
   ],
   exports: [
-    SvgIconComponent,
     FormItemComponent,
     EditFormComponent
   ]

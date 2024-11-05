2657275ac172568c922b803a7af4eedc77d4de9f
SergiyGenza <63913766+SergiyGenza@users.noreply.github.com>
2024-11-05 15:15:44 +0200

    feat: add test info area

--- src/app/modules/todo/ui/todo-list-item-ui/todo-list-item-ui.component.ts
+++ src/app/modules/todo/ui/todo-list-item-ui/todo-list-item-ui.component.ts
@@ -1,14 +1,15 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { Todo } from '../../common/models/todo.model';
 import { SvgIconComponent } from 'angular-svg-icon';
+import { DatePipe } from '@angular/common';
 
 @Component({
-    selector: 'app-todo-list-item-ui',
-    templateUrl: './todo-list-item-ui.component.html',
-    styleUrls: ['./todo-list-item-ui.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: true,
-    imports: [SvgIconComponent]
+  selector: 'app-todo-list-item-ui',
+  templateUrl: './todo-list-item-ui.component.html',
+  styleUrls: ['./todo-list-item-ui.component.scss'],
+  standalone: true,
+  imports: [SvgIconComponent, DatePipe],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TodoListItemUiComponent implements OnChanges {
   @Input()
@@ -17,6 +18,7 @@ export class TodoListItemUiComponent implements OnChanges {
   toggle = new EventEmitter<void>();
 
   listClasses!: string[];
+  openInfo: boolean = false;
 
   ngOnChanges(changes: SimpleChanges): void {
     this.listClasses = [this.todo?.priority!];

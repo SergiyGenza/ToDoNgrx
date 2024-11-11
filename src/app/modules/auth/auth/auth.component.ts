6ea11dae668100ecd039575b59609d962da7fd4a
SergiyGenza <63913766+SergiyGenza@users.noreply.github.com>
2024-11-11 18:58:26 +0200

    fixing conflicts

--- src/app/modules/auth/auth/auth.component.ts
+++ src/app/modules/auth/auth/auth.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html',
     styleUrl: './auth.component.scss',
-    standalone: true,
-    imports: [RouterLink]
+    standalone: true
 })
 export class AuthComponent {
 
---
+++ src/app/modules/auth/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { authRoutes } from '../auth.routes';
+import { AuthComponent } from './auth.component';
+import { AuthService } from '../services/auth.service';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from 'environment/environment';
+
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        // RouterModule.forChild(authRoutes),
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFirestoreModule,
+        AuthComponent,
+    ],
+    providers: [
+        AuthService
+    ]
+})
+export class AuthModule { }
